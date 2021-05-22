5d92c54e63a551a372a2fa1fdde68cea6f5bc28a
Dima <dimhukys@gmail.com>
2021-05-22 18:05:27 +0300

    pagination edit

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,7 @@ class Post < ApplicationRecord
   
   self.per_page = 10
   validates :title, presence: true
+  validates :image, presence: true
   validates :title, :length => {:minimum => 2}
   validates :title, :uniqueness => true
   has_many :comments, dependent: :destroy
