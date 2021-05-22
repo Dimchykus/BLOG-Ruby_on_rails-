28033e0afae0939e1919da1a2647a9dd18a629b9
Dima <dimhukys@gmail.com>
2021-05-22 11:29:42 +0300

    deploying to heroku

--- config/routes.rb
+++ config/routes.rb
@@ -5,13 +5,13 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  # get '/', to: 'posts#index'
+   get '/', to: 'posts#index'
   # get '/posts', to: 'posts#index', as: :posts
   # post '/posts', to: 'posts#create'
   # get '/posts/new', to: 'posts#new', as: :new_post
   # get '/posts/:id', to: 'posts#show',  as: :post
   # get '/posts/:id/edit', to: 'posts#edit',  as: :edit_post
   # patch '/posts/:id', to: 'posts#update'
-  # delete '/posts/:id', to: 'posts#destroy'
+   delete '/posts/:id', to: 'posts#destroy'
  
 end
