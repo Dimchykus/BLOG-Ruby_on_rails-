574b764d13cab8a7c549236633da6c6bf6680d22
Dima <dimhukys@gmail.com>
2021-05-24 22:09:11 +0300

    hqroku 1.5

--- db/migrate/20210522141337_add_username_to_users.rb
+++ db/migrate/20210522141337_add_username_to_users.rb
@@ -1,6 +1,6 @@
 class AddUsernameToUsers < ActiveRecord::Migration[6.1]
   def change
-    add_column  :username, :string, default: ""
-    add_index :username, unique: true
+    add_column  :username, :string, null: false, default: ""
+    add_index :username, null: false, default: "", unique: true
   end
 end
